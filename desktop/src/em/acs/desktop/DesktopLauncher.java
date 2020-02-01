2cc324ec4ee034b61323b52017203dca4197d379
SeanFelipe <sean@addlightness.tech>
2020-02-01 08:41:50 -0800

    initial libgdx jsp-java emacs study

---
+++ desktop/src/em/acs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package em.acs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import em.acs.Revolutionary;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Revolutionary(), config);
+	}
+}
